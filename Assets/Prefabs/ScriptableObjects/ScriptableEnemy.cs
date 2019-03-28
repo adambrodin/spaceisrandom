11bebedc6d059fe16f9557c62721928205e8dd2e
unknown <worecraft@gmail.com>
2019-03-28 09:58:44 +0100

    Initial commit

---
+++ Assets/Prefabs/ScriptableObjects/ScriptableEnemy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyObject", menuName = "EnemyObject")]
+public class EnemyObject : ScriptableObject
+{
+    public int damage, health;
+    public float moveSpeed, projectileSpeed, shootCooldown;
+}
