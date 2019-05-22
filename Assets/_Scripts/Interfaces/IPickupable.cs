0c6441ca80b03a5cf64defd190eceb194c68b37d
adambrodin <worecraft@gmail.com>
2019-05-22 10:50:39 +0200

    IPickupable

---
+++ Assets/_Scripts/Interfaces/IPickupable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/* 
+ * Developed by Adam Brodin
+ * https://github.com/AdamBrodin
+ */
+
+public interface IPickupable<T>
+{
+    event Action<T> OnPickUp;
+
+    void PlayPickUpEffect();
+}
